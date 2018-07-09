fbca9096b2c2eb9be787ae76424187be05744c46
Steve Van Essche <steve.vanessche@gmail.com>
2018-07-09 10:43:10 +0200

    Add parameters for HaloContainer and HaloLight

--- class/HaloContainer.js
+++ class/HaloContainer.js
@@ -1,24 +1,32 @@
 class HaloContainer{ // Class qui contient les halo lumineux et va mettre à jour le style de l'élément HTML
-    constructor(p_html){
+    constructor(p_html, p_parameters = undefined){
         this.container = p_html;
         this.haloLights = [];
         this.direction = 0;
+        this.haloColors = undefined;
+        this.backgroundColors = undefined;
+        this.parameters = undefined;
+
+        if(p_parameters !== undefined){
+            this.parameters = p_parameters;
+        }
+
         // On ajoute les halo lumineux au containers
         const nbreOfHalo = random(8, 12);
         
         while(this.haloLights.length < nbreOfHalo){
-            this.haloLights.push( new HaloLight() );
+            this.createNewHalo();
         }
     }
 
-    update(){
+    update(){ // À appeller à chaque frame pour que le container fonctionne correctement
         this.direction += 0.1;
 
         for(let i = (this.haloLights.length - 1); i >= 0; i--){
             this.haloLights[i].update();
         }
     }
-    render(){
+    render(){ // À appeller pour afficher le résultat en background de l'élément HTML
         let backgroundStyle = "";
 
         for(let i = (this.haloLights.length - 1); i >= 0; i--){
@@ -29,24 +37,72 @@ class HaloContainer{ // Class qui contient les halo lumineux et va mettre à jou
 
         this.container.style.backgroundImage = backgroundStyle;
     }
+    defineColors( newColors ){
+        if(this.haloColors === undefined || this.haloColors === null){
+            this.haloColors = [];
+        }
+
+        if(typeof(newColors) == "string"){
+            this.haloColors.push(newColors);
+        }
+        else if(typeof(newColors) == "object"){
+            // Todo
+        }
+        return false;
+    }
+    defineBackgroundColors( newBackgroundColors){
+        if(this.backgroundColors === undefined || this.backgroundColors === null){
+            this.backgroundColors = [];
+        }
+
+        if(typeof(newBackgroundColors) == "string"){
+            this.backgroundColors.push(newBackgroundColors);
+        }
+        else if(typeof(newBackgroundColors) == "object"){
+            // Todo
+        }
+        return false;
+    }
+
+    createNewHalo(parameters = undefined){
+        if(parameters !== undefined){
+            this.haloLights.push( new HaloLight(parameters) );
+        }
+        else if(this.parameters !== undefined){
+            this.haloLights.push( new HaloLight(this.parameters) );
+        }
+        else{
+            this.haloLights.push( new HaloContainer() );
+        }
+    }
 }
 
 class HaloLight{ // Class qui représente un halo lumineux
-    constructor(){
+    constructor(parameters){
         this.x = random(0, 100);
         this.y = random(0, 100);
 
         this.size = random(1, 25);
-        // this.speed = random(0.01, 0.1);
         this.speed = (25 - this.size) / 128;
 
         this.direction = Math.round(Math.random() * 35) + 10;
-        this.direction = radians(this.direction);
+        
 
         this.red = Math.round(Math.random() * 20 + 100);
         this.green = Math.round(Math.random() * 20 + 200);
         this.blue = Math.round(Math.random() * 20 + 235);
         this.alpha = ((25 - (this.size)) / 50);
+
+        if(parameters !== undefined && parameters !== null && typeof(parameters) === "object" ){
+            const parametersKey = ['x', 'y', 'size', 'speed', 'direction', 'red', 'green', 'blue', 'alpha'];
+            parametersKey.forEach((key)=>{
+                if(key in parameters){
+                    this[key] = parameters[key];
+                }
+            });
+        }
+
+        this.direction = radians(this.direction);
     }
 
     update(){
